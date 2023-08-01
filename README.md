6cf3835c0183ef5d1264a066dba353235d7233bf
Years <89029641+HX-Years@users.noreply.github.com>
2023-08-01 02:14:40 +0000

    commit

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 # 终维度模组
 一个Mindustry大型模组，目前处于初期，框架大致已经设计完成，准备实现。本人学生，其开发进度缓慢。
 # 关于群聊
-qq群: 193568024 
+暂无
 # 个人的话
 慢慢来吧
 > 本mod需要一个前置mod：[Universe Core](https://github.com/EB-wilson/UniverseCore)，
--- src/ultimatedimension/UltimateDimension.java
+++ src/ultimatedimension/UltimateDimension.java
@@ -9,17 +9,17 @@ import universecore.annotations.Annotations;
 public class UltimateDimension extends Mod{
 	public static final String ModName = "ultimate-dimension";
 	public static final ContentList[] modContents = new ContentList[]{
-		new ultimatedimension.content.UDItems(),
-		new ultimatedimension.content.UDLiquids(),
+		new UDItems(),
+		new UDLiquids(),
 		//new NDPlanets(),
 		//new NDSectorPresets(),
 		//new NDCoreBlocks(),
-		new ultimatedimension.content.UDTurrets(),
-		new ultimatedimension.content.UDDefenseBlocks(),
-		new ultimatedimension.content.UDCrafterBlocks(),
-		new ultimatedimension.content.UDPowerBlocks(),
-		new ultimatedimension.content.UDOreBlocks(),
-		new ultimatedimension.content.UDTechTree()
+		new UDTurrets(),
+		new UDDefenseBlocks(),
+		new UDCrafterBlocks(),
+		new UDPowerBlocks(),
+		new UDOreBlocks(),
+		new UDTechTree()
 	};
 	public UltimateDimension(){
 		Log.info(logInfo("Loaded UltimateDimension constructor."));
